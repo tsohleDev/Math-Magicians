4531e93485ec1eb2754296effb25a6d028a84852
tsohleDev <mokhemisitsohle@gmail.com>
2023-01-12 17:46:29 +0200

    Impliment the logic to the calculator Use props and state and state up

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,20 +3,23 @@ import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
-    const { text } = this.props;
+    const { text, handleClick } = this.props;
 
-    function setClass() {
+    function setClass(text) {
       let styleClass = 'btn';
       if (text === '0') { styleClass = 'naught'; } else if (text === '+' || text === '-' || text === 'x' || text === '=') { styleClass = 'operand'; }
       return styleClass;
     }
 
-    return <button type="button" className={setClass()}>{text}</button>;
+    return <button type="button" onClick={() => { handleClick(text); }} className={setClass(text)}>{text}</button>;
   }
 }
 
+// validate the props
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+
 };
 
 export default Button;
--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Screen extends React.Component {
   render() {
+    const { total, next, operation } = this.props;
+
+    const handleDisplay = () => {
+      let string = ''; let
+        assigned = false;
+
+      if (total) { string += total; assigned = true; }
+      if (operation) { string += operation; assigned = true; }
+      if (next) { string += next; assigned = true; }
+
+      if (!assigned) return '0';
+      return string;
+    };
+
     return (
       <div className="screen">
-        0
+        <p>{handleDisplay()}</p>
       </div>
     );
   }
 }
+
+Screen.propTypes = {
+  total: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+};
 
 export default Screen;
