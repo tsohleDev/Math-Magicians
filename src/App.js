a210f475a0668690d8d163ad21eae2db01832365
tsohleDev <mokhemisitsohle@gmail.com>
2023-01-19 14:48:16 +0200

    Use React Router
    Create componrnt for home at /
    Create CalculatorPage at /calculator
    Create Qoute at /Qoute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Calculator from './Components/Calculator';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home/Home';
 import './App.css';
+import Qoute from './Components/Qoute/Qoute';
+import CalculatorPage from './Components/CalculatorPage/CalculatorPage';
 
-class App extends React.Component {
-  render() {
-    return <Calculator />;
-  }
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Math Magicians</h1>
+
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/quote">Quote</Link></li>
+        </ul>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<Qoute />} />
+      </Routes>
+
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/CalculatorPage/CalculatorPage.js
@@ -0,0 +1,13 @@
+import Calculator from '../Calculator';
+import './CalculatorPage.css';
+
+function CalculatorPage() {
+  return (
+    <section className="calcpage">
+      <h1>Lets do some math!</h1>
+      <Calculator />
+    </section>
+  );
+}
+
+export default CalculatorPage;
